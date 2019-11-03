39fb298fde6efa11d83e50c2467f851a7c3fe6e3
Daniel Garnier-Moiroux <git@garnier.wf>
2019-11-03 15:12:31 +0100

    Extract names from swapi response

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,30 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import reactor.netty.http.client.HttpClient
 
 val baseUrl = "https://swapi.co/api"
 
 fun main() {
+    val mapper = jacksonObjectMapper()
     println("Hello world !")
-    val first = HttpClient.create()
+    val names = HttpClient.create()
             .baseUrl(baseUrl)
             .get()
             .uri("/people/")
             .responseContent()
+            .aggregate()
             .asString()
-            .blockFirst()
+            .map { mapper.readValue(it, PeopleResponse::class.java) }
+            .map { it.results }
+            .block()!!
+            .map { it.name }
 
-    println(first)
+    println(names.joinToString(",\n"))
 }
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class PeopleResponse(val results: List<Person>) {
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Person(val name: String) {}
