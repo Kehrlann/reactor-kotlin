3ab4d73952f31dcbb9be390360eefe345809ed15
Daniel Garnier-Moiroux <git@garnier.wf>
2019-11-04 11:14:22 +0100

    List films for Luke

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,43 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import reactor.core.publisher.Flux
 import reactor.netty.http.client.HttpClient
 
 val baseUrl = "https://swapi.co/api"
+val client = HttpClient.create()
+        .baseUrl(baseUrl)
 
 fun main() {
     val mapper = jacksonObjectMapper()
     println("Hello world !")
-    val names = HttpClient.create()
-            .baseUrl(baseUrl)
+    val films = client
             .get()
-            .uri("/people/")
+            .uri("/people/?search=luke")
             .responseContent()
             .aggregate()
             .asString()
             .map { mapper.readValue(it, PeopleResponse::class.java) }
-            .map { it.results }
-            .block()!!
-            .map { it.name }
+            .flatMapIterable { it.results }
+            .flatMapIterable { it.films }
+            .flatMap {
+                client.get()
+                        .uri(it)
+                        .responseContent()
+                        .aggregate()
+                        .asString()
+            }
+            .map { mapper.readValue(it, Film::class.java).title }
+            .toIterable()
+            .joinToString(",\n")
 
-    println(names.joinToString(",\n"))
+    println(films)
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class PeopleResponse(val results: List<Person>) {
-}
+class PeopleResponse(val results: List<Person>)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Person(val name: String) {}
+data class Person(val name: String, val films: List<String>)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Film(val title: String, val characters: List<String>)
