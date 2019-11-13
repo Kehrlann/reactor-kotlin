ab91619201d17b253c5e465258cf8bac12090001
Daniel Garnier-Moiroux <git@garnier.wf>
2019-11-13 08:48:49 +0100

    WiP: trying out the context

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
+import reactor.util.context.Context
+import java.util.*
 
 val baseUrl = "https://swapi.co/api"
 val client = HttpClient.create()
@@ -19,6 +21,13 @@ fun main() {
             .asString()
             .map { mapper.readValue(it, PeopleResponse::class.java) }
             .flatMapIterable { it.results }
+            .doOnNext { person ->
+                println("ON next $person")
+                Mono.subscriberContext()
+                        .doOnNext{ ctx -> println("got context !") }
+                        .map { ctx -> ctx.put(person.url, Mono.just(person)) }
+                        .doOnNext { ctx -> println("put ${person.url} in cache, ${ctx.hasKey(person.url)}") }
+            }
             .flatMapIterable { it.films }
             .flatMap { getFilm(it) }
             .flatMap { getCharactersForFilm(it) }
@@ -33,7 +42,7 @@ fun main() {
 class PeopleResponse(val results: List<Person>)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Person(val name: String, val films: List<String>)
+data class Person(val name: String, val films: List<String>, val url: String)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Film(val title: String, val characters: List<String>) {
@@ -46,9 +55,23 @@ class Film(val title: String, val characters: List<String>) {
 
 fun getCharactersForFilm(baseFilm: Film): Mono<Film> {
     return Flux.fromIterable(baseFilm.characters)
-            .flatMap {
-                getPerson(it)
-//                        .doOnNext { p -> println("Got ${p.name} for film ${baseFilm.title}") }
+            .flatMap { url ->
+                println("Getting $url")
+                Mono.subscriberContext()
+                        .map { ctx ->
+                            ctx.getOrEmpty<Mono<Person>>(url)
+                                    .map { mp ->
+                                        println("got $url from cache")
+                                        mp
+                                    }
+                                    .orElseGet {
+                                        println("${System.currentTimeMillis()} new request for $url")
+                                        val newReq = getPerson(url)
+                                        ctx.put(url, newReq)
+                                        newReq
+                                    }
+                        }
+                        .flatMap { it }
             }
             .reduce(listOf<String>(), { list, person -> list.plus(person.name) })
             .map { Film(baseFilm.title, it) }
@@ -61,6 +84,7 @@ private fun getPerson(url: String): Mono<Person> {
             .aggregate()
             .asString()
             .map { mapper.readValue<Person>(it, Person::class.java) }
+            .cache()
 }
 
 
@@ -71,5 +95,4 @@ private fun getFilm(url: String): Mono<Film> {
             .aggregate()
             .asString()
             .map { mapper.readValue(it, Film::class.java) }
-//            .doOnNext { println("Got film: ${it.title}") }
 }
