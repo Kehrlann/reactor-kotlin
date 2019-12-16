70372db164156a046ed9dc2c75868d65e1bf3ae4
Daniel Garnier-Moiroux <git@garnier.wf>
2019-12-16 16:55:48 +0100

    Show several attempts at getting a paginated range

--- src/main/kotlin/ListAllCharacters.kt
+++ src/main/kotlin/ListAllCharacters.kt
@@ -6,25 +6,13 @@ fun main(args: Array<String>) {
     println("Listing all characters ...")
     println("")
 
-    val result = getPage(1)
-            .expand { it ->
-                if (it.isFirst) {
-                    // If it is the first page, load all the other ones
-                    val numberOfPages = it.count / it.results.count()
-                    Flux.range(2, numberOfPages)
-                            .flatMap {
-                                getPage(it)
-                            }
-                } else {
-                    // If it is a subsequent page, don't load anything
-                    Flux.empty()
-                }
-            }
+    val result = getAllPages()
             .map { it.results }
             .flatMapIterable { it }
             .collectList()
             .block()
 
+    println("")
     println("Total characters: ${result?.size ?: 0}")
     println("")
 
@@ -42,4 +30,43 @@ private fun getPage(pageNumber: Int): Mono<PaginatedResource<Person>> {
             .aggregate()
             .asString()
             .map { mapper.readValue<PaginatedResource<Person>>(it) }
+            .doOnSubscribe { println("Requesting page #$pageNumber ...") }
 }
+
+private fun getPageRange(start: Int, count: Int) =
+        Flux.range(start, count).flatMap { getPage(it) }
+
+// This function gets the first page, then from that, derives the total number of pages, then gets them all
+// Note: it downloads the first page twice.
+private fun getAllPagesNaive(): Flux<PaginatedResource<Person>> =
+        getPage(1)
+                .map { it.numberOfPages }
+                .flatMapMany { Flux.range(1, it) }
+                .flatMap { getPage(it) }
+
+// This functions gets the first page and then the subsequent pages, using expand.
+// However, it requires modifying the "paginatedResponse" to have a flag "isFirst"
+private fun getAllPagesNoDuplication(): Flux<PaginatedResource<Person>> =
+        getPage(1)
+                .expand { it ->
+                    if (it.isFirst) {
+                        // If it is the first page, load the reste of the pages
+                        Flux.range(2, it.numberOfPages - 1)
+                                .flatMap { getPage(it) }
+                    } else {
+                        // If it is a subsequent page, don't load anything
+                        Flux.empty()
+                    }
+                }
+
+
+// This one gets all pages without needing to add "isFirst" in the paginated response
+// and is the most explicit about what it does
+private fun getAllPages() =
+        getPage(1)
+                .flatMapMany {
+                    Mono
+                            .just(it)
+                            .concatWith(getPageRange(2, it.numberOfPages - 1))
+                }
+
