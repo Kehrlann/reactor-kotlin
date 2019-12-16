665c7cacde161362c84fc8e8a27eb8b1e779a3a2
Daniel Garnier-Moiroux <git@garnier.wf>
2019-12-16 16:45:57 +0100

    Fix "number of pages" count

--- src/main/kotlin/PaginatedResource.kt
+++ src/main/kotlin/PaginatedResource.kt
@@ -1,3 +1,6 @@
+import kotlin.math.ceil
+
 class PaginatedResource<T>(val count: Int, val next: String?, val previous: String?, val results: List<T>) {
     val isFirst = previous == null
+    val numberOfPages = ceil(count.toDouble() / results.count()).toInt()
 }
