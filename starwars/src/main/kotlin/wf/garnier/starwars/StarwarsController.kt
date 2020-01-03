ff32ddeb3696a25f4d685c0bbbb51532d5c8906d
Daniel Garnier-Moiroux <git@garnier.wf>
2020-01-03 16:41:50 +0100

    Add atomic integer to count number of requests in flight

--- starwars/src/main/kotlin/wf/garnier/starwars/StarwarsController.kt
+++ starwars/src/main/kotlin/wf/garnier/starwars/StarwarsController.kt
@@ -4,14 +4,19 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 @RequestMapping("api")
 class StarwarsController(private val people: Map<Int, Person>, private val films: Map<Int, Film>) {
 
+    private val currentRequests: AtomicInteger = AtomicInteger(0)
+
     @GetMapping("/people/")
     fun getPeople(): Flux<Person> {
         return Flux.fromIterable(people.values)
+                .doOnSubscribe { currentRequests.incrementAndGet() }
+                .doOnTerminate { currentRequests.decrementAndGet() }
     }
 
     @GetMapping("/people/{id}/")
@@ -19,6 +24,8 @@ class StarwarsController(private val people: Map<Int, Person>, private val films
         return Mono.justOrEmpty(people[id])
                 .map { ResponseEntity.ok().body(it) }
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
+                .doOnSubscribe { currentRequests.incrementAndGet() }
+                .doOnTerminate { currentRequests.decrementAndGet() }
     }
 
     @GetMapping("/films/{id}/")
@@ -26,5 +33,7 @@ class StarwarsController(private val people: Map<Int, Person>, private val films
         return Mono.justOrEmpty(films[id])
                 .map { ResponseEntity.ok().body(it) }
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
+                .doOnSubscribe { currentRequests.incrementAndGet() }
+                .doOnTerminate { currentRequests.decrementAndGet() }
     }
 }
